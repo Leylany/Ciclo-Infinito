fb38fd51676b6bf5d4bb8eba222e264bc930a198
Leylany <36907439+Leylany@users.noreply.github.com>
2018-04-17 20:31:51 -0500

    Add files via upload

---
+++ CicloInfinito.java
@@ -0,0 +1,11 @@
+public class CicloInfinito
+{
+    public static void main(String args[])
+    { 
+        do
+        {
+         System.out.println("CICLO INFINITO");
+        }
+        while(true);
+    }
+}
